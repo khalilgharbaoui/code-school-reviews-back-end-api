7babceaa1e5e0b7b65af67d20557089fef3c874c
khalilgharbaoui <kaygeee@gmail.com>
2016-03-10 01:20:38 +0100

    added a failing controller test

--- spec/controllers/codeschool_controller_spec.rb
+++ spec/controllers/codeschool_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+include FactoryGirl::Syntax::Methods
 
 RSpec.describe CodeschoolController, type: :controller do
+
+build(:codeschool)
+
+
+
+
+  describe "GET #index" do
+    it "gives a JSON of all tasks" do
+      get :index, format: :json
+    end
+  end
+
 
 end
