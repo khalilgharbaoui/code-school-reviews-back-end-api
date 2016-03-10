9e811838b7866838c1ce00582dbd8a670a2fdf2c
khalilgharbaoui <kaygeee@gmail.com>
2016-03-10 01:44:43 +0100

    added controller tests

--- spec/controllers/codeschool_controller_spec.rb
+++ spec/controllers/codeschool_controller_spec.rb
@@ -9,10 +9,33 @@ build(:codeschool)
 
 
   describe "GET #index" do
-    it "gives a JSON of all tasks" do
+    it "gives a JSON of all codeschools" do
       get :index, format: :json
     end
+
+    it "returns json" do
+         get :index
+         expect(response.header['Content-Type']).to include('application/json')
+       end
   end
+
+  describe "GET #show" do
+    it "gives a JSON of one codeschool" do
+      get :show, format: :json
+    end
+  end
+
+  describe "POST #create" do
+    it "creates a new Codeschool" do
+       expect {post :create}.to change(Codeschool, :count).by(1)
+     end
+   end
+
+   describe "DELETE #destroy" do
+     it "remove a Codeschool" do
+        expect {delete :destroy}.to change(Codeschool, :count).by(-1)
+      end
+    end
 
 
 end
