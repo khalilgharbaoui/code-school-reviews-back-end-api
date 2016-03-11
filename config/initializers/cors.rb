744a9e77a12ddbf3890ed853bb3844f12b6b2374
khalilgharbaoui <kaygeee@gmail.com>
2016-03-11 01:13:25 +0100

    fixed cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class CodeSchoolReviews::Application
   config.middleware.insert_before 0, "Rack::Cors", debug: true, logger: (-> { Rails.logger}) do
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000',
+      origins 'http://localhost:3000', 'http://127.0.0.1:3000',
       /\Ahttp:\/\/192\.168.\.0\.\d{1,3}(:\d+)?\z/
 
       resource 'cors',
