9ca0f0ae4705e9ff6dcbb21d4611fc276fccf21c
khalilgharbaoui <kaygeee@gmail.com>
2016-03-11 06:01:16 +0100

    fixing

--- app/models/codeschool.rb
+++ app/models/codeschool.rb
@@ -1,7 +1,6 @@
 class Codeschool < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
-  #mount_uploader :logo, LogoUploader
-  mount_base64_uploader :logo, LogoUploader
+
 
   def average_rating
     # self.reviews.average(:rating).round if self.reviews.present?
