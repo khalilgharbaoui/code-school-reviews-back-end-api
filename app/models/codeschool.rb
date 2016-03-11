ef5503e420b5aa55983f2d56ad10de66495b61b8
khalilgharbaoui <kaygeee@gmail.com>
2016-03-11 06:16:48 +0100

    protect_from_forgery with: :exception

--- app/models/codeschool.rb
+++ app/models/codeschool.rb
@@ -1,6 +1,7 @@
 class Codeschool < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
-
+  #mount_uploader :logo, LogoUploader
+  mount_base64_uploader :logo, LogoUploader
 
   def average_rating
     # self.reviews.average(:rating).round if self.reviews.present?
